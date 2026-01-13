6ef9a4dfefe4c64a71c9ee3b848d0e93daad4f7a
Clement Venot <clement.venooot@gmail.com>
2026-01-13 12:00:51 +0100

    add template multi module from github.com/clementvenot

---
+++ template-parent/template-frontend/src/main/java/com/template/template_frontend/TemplateFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.template.template_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateFrontendApplication.class, args);
+	}
+
+}
---
+++ template-parent/template-frontend/src/test/java/com/template/template_frontend/TemplateFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.template_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
