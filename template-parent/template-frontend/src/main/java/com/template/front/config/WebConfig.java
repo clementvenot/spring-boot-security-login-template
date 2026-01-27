0effaed8cc7e5770f3ea73f6269907c3d9b8f35b
Clement Venot <clement.venooot@gmail.com>
2026-01-27 23:09:07 +0100

    feat(WebConfig) : create cookie for langage

--- template-parent/template-frontend/src/main/java/com/template/front/config/WebConfig.java
+++ template-parent/template-frontend/src/main/java/com/template/front/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.template.front.config;
 
 import com.template.front.web.LoginRequiredInterceptor;
+import java.util.Locale;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import java.time.Duration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -22,5 +29,34 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginRequiredInterceptor())
                 .addPathPatterns("/secure/**", "/secure");
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setCookieName("LOCALE");
+        clr.setDefaultLocale(Locale.FRENCH);
+        clr.setCookieMaxAge(Duration.ofDays(365));
+        clr.setCookieSecure(true);           
+        clr.setCookieHttpOnly(false);    
+        clr.setCookieSameSite("Lax");
+        return clr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); // ex: ?lang=en or ?lang=fr
+        return lci;
+    }
+    
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        return messageSource;
     }
 }
