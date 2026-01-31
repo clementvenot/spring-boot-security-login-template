bde4e17da1db5708feb34a9d59e3067e395f73c7
Clement Venot <clement.venooot@gmail.com>
2026-01-31 21:45:41 +0100

    fix (SecurityConfig) : add endpoint forgotpwd and reset pwd

--- template-parent/template-backend/src/main/java/com/template/config/SecurityConfig.java
+++ template-parent/template-backend/src/main/java/com/template/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 			    .requestMatchers("/auth/login", "/auth/register", "/auth/logout",
-			                     "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+			                     "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+			                     "/auth/forgot-password", "/auth/reset-password")
 			    .permitAll()
 			    .anyRequest().authenticated()
 			)
